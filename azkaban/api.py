a9c79f908d8c84d1f26e565d67a37f5a4749e706
Christophe Préaud <christophe.preaud@kelkoogroup.com>
2021-05-20 17:49:31 +0200

    Refactor to use requests instead of urllib

--- azkaban/api.py
+++ azkaban/api.py
@@ -1,66 +1,66 @@
 import json
 import requests
 from os.path import basename
-from urllib.error import URLError
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
+from requests.exceptions import RequestException
 
 def authenticate(server_host, server_port, user_name, password):
-    parameters = urlencode({
-        'action':'login',
-        'username':user_name,
-        'password':password
-    })
-    parameters = parameters.encode('ascii')
+    parameters = {
+        'action': 'login',
+        'username': user_name,
+        'password': password
+    }
     azkaban_url = f'http://{server_host}:{server_port}'
-    with urlopen(azkaban_url, parameters) as fp:
-        return json.loads(fp.read())
+    r = requests.post(azkaban_url, data=parameters)
+    return _get_json_from_response(r)['session.id']
 
 def create_project(server_host, server_port, session_id, project_name):
-    parameters = urlencode({
-        'action':'create',
-        'session.id':session_id,
-        'name':project_name,
-        'description':f'{project_name}_project'
-    })
-    parameters = parameters.encode('ascii')
+    parameters = {
+        'action': 'create',
+        'session.id': session_id,
+        'name': project_name,
+        'description': f'{project_name}_project'
+    }
     azkaban_url = f'http://{server_host}:{server_port}/manager'
-    with urlopen(azkaban_url, parameters) as fp:
-        response = json.loads(fp.read())
-        if 'status' in response and response['status'] == 'success':
-            return(response)
-        else:
-            raise URLError(response['message'])
+    r = requests.post(azkaban_url, params=parameters)
+    return _get_json_from_response(r)
 
 def delete_project(server_host, server_port, session_id, project_name):
-    parameters = urlencode({
-        'delete':'true',
-        'session.id':session_id,
-        'project':project_name
-    })
-    azkaban_url = f'http://{server_host}:{server_port}/manager?{parameters}'
-    urlopen(azkaban_url)
+    parameters = {
+        'delete': 'true',
+        'session.id': session_id,
+        'project': project_name
+    }
+    azkaban_url = f'http://{server_host}:{server_port}/manager'
+    _call_ws_get(azkaban_url, parameters, return_json=False)
+
+def fetch_flow_jobs(server_host, server_port, session_id, project_name, flow_name):
+    parameters = {
+        'ajax': 'fetchflowgraph',
+        'session.id': session_id,
+        'project': project_name,
+        'flow': flow_name
+    }
+    azkaban_url = f'http://{server_host}:{server_port}/manager'
+    return _call_ws_get(azkaban_url, parameters)
 
 def fetch_project_flows(server_host, server_port, session_id, project_name):
-    parameters = urlencode({
-        'ajax':'fetchprojectflows',
-        'session.id':session_id,
-        'project':project_name
-    })
-    azkaban_url = f'http://{server_host}:{server_port}/manager?{parameters}'
-    with urlopen(azkaban_url) as fp:
-        return json.loads(fp.read())
+    parameters = {
+        'ajax': 'fetchprojectflows',
+        'session.id': session_id,
+        'project': project_name
+    }
+    azkaban_url = f'http://{server_host}:{server_port}/manager'
+    return _call_ws_get(azkaban_url, parameters)
 
 def fetch_schedule(server_host, server_port, session_id, flow_id, project_id):
-    parameters = urlencode({
-        'ajax':'fetchSchedule',
-        'session.id':session_id,
-        'flowId':flow_id,
-        'projectId':project_id
-    })
-    azkaban_url = f'http://{server_host}:{server_port}/schedule?{parameters}'
-    with urlopen(azkaban_url) as fp:
-        return json.loads(fp.read())
+    parameters = {
+        'ajax': 'fetchSchedule',
+        'session.id': session_id,
+        'flowId': flow_id,
+        'projectId': project_id
+    }
+    azkaban_url = f'http://{server_host}:{server_port}/schedule'
+    return _call_ws_get(azkaban_url, parameters)
 
 def get_flows_id(server_host, server_port, session_id, project_name):
     flows = fetch_project_flows(server_host, server_port, session_id, project_name)
@@ -77,37 +77,27 @@ def get_project_id(server_host, server_port, session_id, project_name):
         return flows['projectId']
 
 def schedule_cron_flow(server_host, server_port, session_id, project_name, flow_name, cron_expression):
-    parameters = urlencode({
-        'ajax':'scheduleCronFlow',
-        'session.id':session_id,
-        'projectName':project_name,
-        'flow':flow_name,
-        'failureAction':'finishPossible',
-        'cronExpression':f'0 {cron_expression}'
-    })
-    parameters = parameters.encode('ascii')
+    parameters = {
+        'ajax': 'scheduleCronFlow',
+        'session.id': session_id,
+        'projectName': project_name,
+        'flow': flow_name,
+        'failureAction': 'finishPossible',
+        'cronExpression': f'0 {cron_expression}'
+    }
     azkaban_url = f'http://{server_host}:{server_port}/schedule'
-    with urlopen(azkaban_url, parameters) as fp:
-        response = json.loads(fp.read())
-        if 'status' in response and response['status'] == 'success':
-            return(response)
-        else:
-            raise URLError(response['message'])
+    r = requests.post(azkaban_url, params=parameters)
+    return _get_json_from_response(r)
 
 def unschedule_flow(server_host, server_port, session_id, schedule_id):
-    parameters = urlencode({
-        'action':'removeSched',
-        'session.id':session_id,
-        'scheduleId':schedule_id
-    })
-    parameters = parameters.encode('ascii')
+    parameters = {
+        'action': 'removeSched',
+        'session.id': session_id,
+        'scheduleId': schedule_id
+    }
     azkaban_url = f'http://{server_host}:{server_port}/schedule'
-    with urlopen(azkaban_url, parameters) as fp:
-        response = json.loads(fp.read())
-        if 'status' in response and response['status'] == 'success':
-            return(response)
-        else:
-            raise URLError(response['message'])
+    r = requests.post(azkaban_url, params=parameters)
+    return _get_json_from_response(r)
 
 def upload_project(server_host, server_port, session_id, project_name, zip_file):
     azkaban_url = f'http://{server_host}:{server_port}/manager'
@@ -115,26 +105,35 @@ def upload_project(server_host, server_port, session_id, project_name, zip_file)
         'file': (basename(zip_file), open(zip_file, 'rb'), 'application/zip')
     }
     parameters = {
-        'ajax':'upload',
-        'session.id':session_id,
-        'project':project_name
+        'ajax': 'upload',
+        'session.id': session_id,
+        'project': project_name
     }
-    requests.post(azkaban_url, files=files, data=parameters)
+    r = requests.post(azkaban_url, files=files, data=parameters)
+    r.raise_for_status()
     flows_id = get_flows_id(server_host, server_port, session_id, project_name)
     if len(flows_id) == 0:
-        raise URLError(f"Project '{project_name}' push on Azkaban failed")
+        raise RequestException(f"AzkabanError: Project '{project_name}' push on Azkaban failed")
 
-#    file_data = open(zip_file, 'rb')
-#    parameters = urlencode({
-#        'ajax':'upload',
-#        'session.id':session_id,
-#        'file':f'{file_data};type=application/zip',
-#        'project':project_name
-#    })
-#    parameters = parameters.encode('ascii')
-#    azkaban_url = f'http://{server_host}:{server_port}/manager'
-#    #headers = {'Content-Type':'multipart/mixed'}
-#    req = Request(url = azkaban_url, data = parameters) #, headers = headers)
-#    with urlopen(req) as fp:
-#        #return json.loads(fp.read())
-#        print(fp.read())
+def _call_ws_get(azkaban_url, parameters, return_json=True):
+    r = requests.get(azkaban_url, params=parameters)
+    r.raise_for_status()
+    if return_json:
+        json_response = r.json()
+        if 'error' in json_response:
+            raise RequestException(f"AzkabanError: {json_response['error']}")
+        else:
+            return r.json()
+    
+def _get_json_from_response(r):
+    r.raise_for_status()
+    json_response = r.json()
+    if 'status' in json_response and json_response['status'] == 'success':
+        return json_response
+    else:
+        if 'error' in json_response:
+            raise RequestException(f"AzkabanError: {json_response['error']}")
+        elif 'message' in json_response:
+            raise RequestException(f"AzkabanError: {json_response['message']}")
+        else:
+            raise RequestException(f"AzkabanError: {json_response}")
--- test.py
+++ test.py
@@ -1,71 +1,55 @@
 #!/usr/bin/python3
 
 import azkaban.api
-import json
 import sys
-from urllib.error import URLError
-from urllib.request import urlopen
-
-def get_active_nn(nn_list):
-    def is_active_nn(nn):
-        jmx_url = "http://{host}:{port}/jmx?qry=Hadoop:service=NameNode,name=NameNodeStatus".format(
-            host = nn['Host'],
-            port = nn['Port']
-        )
-        try:
-            jsonData = json.loads(urlopen(jmx_url).read())
-            return 'active' == jsonData['beans'][0]['State']
-        except URLError as e:
-            pass
-
-    for nn in nn_list:
-        if is_active_nn(nn):
-            return nn
-    sys.exit('ERROR: all NameNodes are DOWN!')
+from requests.exceptions import RequestException
 
 def main():
-    azkaban_hostname = "localhost"
+    azkaban_hostname = "dc1-kdp-dev-test-01.dev.dc1.kelkoo.net"
     azkaban_port = "8081"
-    azkaban_username = "azkaban"
-    azkaban_password = "azkaban"
+    azkaban_username = "test"
+    azkaban_password = "test"
 
-    session_id=""
-    response = azkaban.api.authenticate(azkaban_hostname, azkaban_port, azkaban_username, azkaban_password)
-    if 'status' in response and response['status'] == 'success':
-        session_id = response['session.id']
-    else:
-        sys.exit('ERROR: ' + response)
+    status = 0
+    try:
+        session_id = azkaban.api.authenticate(azkaban_hostname, azkaban_port, azkaban_username, azkaban_password)
+        print(f'session_id: {session_id}')
 
-    print(f'session_id: {session_id}')
+        response = azkaban.api.create_project(azkaban_hostname, azkaban_port, session_id, 'toto')
+        print(response)
 
-    response = azkaban.api.create_project(azkaban_hostname, azkaban_port, session_id, 'toto')
-    print(response)
+#        azkaban.api.delete_project(azkaban_hostname, azkaban_port, session_id, 'toto')
 
-#    response = azkaban.api.delete_project(azkaban_hostname, azkaban_port, session_id, 'toto')
-#    print(response)
+        project_flows = azkaban.api.fetch_project_flows(azkaban_hostname, azkaban_port, session_id, 'example_project_with_api_flow_1_0')
+#        print(project_flows)
 
-#    project_flows = azkaban.api.fetch_project_flows(azkaban_hostname, azkaban_port, session_id, 'project_api_flow_10')
-#    print(project_flows)
+#        for flow in project_flows['flows']:
+#            response = azkaban.api.fetch_schedule(azkaban_hostname, azkaban_port, session_id, flow['flowId'], project_flows['projectId'])
+#            print(f"--> {flow['flowId']}: {response}")
+
+        project_id = azkaban.api.get_project_id(azkaban_hostname, azkaban_port, session_id, 'example_project_with_api_flow_1_0')
+
+        flows_id = azkaban.api.get_flows_id(azkaban_hostname, azkaban_port, session_id, 'example_project_with_api_flow_1_0')
+#        print(flows_id)
 #
-#    for flow in project_flows['flows']:
-#        response = azkaban.api.fetch_schedule(azkaban_hostname, azkaban_port, session_id, flow['flowId'], project_flows['projectId'])
+#        for flow_id in flows_id:
+#            flow_jobs = azkaban.api.fetch_flow_jobs(azkaban_hostname, azkaban_port, session_id, 'example_project_with_api_flow_1_0', flow_id)
+#            print(flow_jobs)
+#
+#            schedule_id = azkaban.api.get_flow_schedule_id(azkaban_hostname, azkaban_port, session_id, flow_id, project_id)
+#            print(f'{flow_id}: {schedule_id}')
+#            if schedule_id is not None:
+#                response = azkaban.api.unschedule_flow(azkaban_hostname, azkaban_port, session_id, schedule_id)
+#                print(response)
+#
+#        response = azkaban.api.schedule_cron_flow(azkaban_hostname, azkaban_port, session_id, 'example_project_with_api_flow_1_0', 'example_flow_with_country_param_it', '50 1 ? * *')
 #        print(response)
 #
-#    project_id = azkaban.api.get_project_id(azkaban_hostname, azkaban_port, session_id, 'project_api_flow_10')
-#
-#    flows_id = azkaban.api.get_flows_id(azkaban_hostname, azkaban_port, session_id, 'project_api_flow_10')
-#
-#    for flow_id in flows_id:
-#        schedule_id = azkaban.api.get_flow_schedule_id(azkaban_hostname, azkaban_port, session_id, flow_id, project_id)
-#        print(f'{flow_id}: {schedule_id}')
-#
-#        if schedule_id is not None:
-#            response = azkaban.api.unschedule_flow(azkaban_hostname, azkaban_port, session_id, schedule_id)
-#            print(response)
-#
-#    response = azkaban.api.schedule_cron_flow(azkaban_hostname, azkaban_port, session_id, 'project_api_flow_10', 'flow10_country_param_it', '50 1 ? * *')
-#    print(response)
+        azkaban.api.upload_project(azkaban_hostname, azkaban_port, session_id, 'toto', '/home/preaudc/dev/git/azkaban-utils/basicFlow20Project.zip')
+    except RequestException as e:
+        status = 1
+        print(e)
 
-    azkaban.api.upload_project(azkaban_hostname, azkaban_port, session_id, 'toto', '/home/cpreaud/dev/git/azkaban-utils/basicFlow20Project.zip')
+    sys.exit(status)
 
 main()
